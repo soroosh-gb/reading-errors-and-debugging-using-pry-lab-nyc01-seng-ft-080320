accee863fc72e32630a3a4dc706ed7262f11f59d
Soroosh Golbabae <soroosh.gb@gmail.com>
2020-06-24 17:36:50 +0000

    Automatically backed up by Learn

--- fix_using_tests/false_equivalency.rb
+++ fix_using_tests/false_equivalency.rb
@@ -1,4 +1,5 @@
 # don't forget to add: require 'pry'
+require 'pry'
 
 def get_user_input
   gets.chomp
@@ -12,9 +13,12 @@ end
 
 def selection(num)
   if num = 1
-     "YUM YUM MUNCH MUNCH MUNCH"
+    binding.pry 
+      "YUM YUM MUNCH MUNCH MUNCH"
+  
   elsif num = 2
-     "HAM HAM HAM IN MY TUMMY"
+  binding.pry 
+    "HAM HAM HAM IN MY TUMMY"
   end
 end
 
