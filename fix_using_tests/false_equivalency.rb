954254b2499ccefd14148afebf62192130f1135c
Soroosh Golbabae <soroosh.gb@gmail.com>
2020-06-24 18:40:00 +0000

    Automatically backed up by Learn

--- fix_using_tests/false_equivalency.rb
+++ fix_using_tests/false_equivalency.rb
@@ -14,13 +14,14 @@ end
 def selection(num)
   
   if num = 1
-    binding.pry 
+    
       "YUM YUM MUNCH MUNCH MUNCH"
     
   elsif num = 2
-   binding.pry  
+    
     "HAM HAM HAM IN MY TUMMY"
   end
+  binding.pry 
 end
 
 def runner
