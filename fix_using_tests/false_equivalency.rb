f86eeb47d5e1bd36496c3980a9b26c74628d1282
Soroosh Golbabae <soroosh.gb@gmail.com>
2020-06-24 17:44:40 +0000

    Automatically backed up by Learn

--- fix_using_tests/false_equivalency.rb
+++ fix_using_tests/false_equivalency.rb
@@ -13,11 +13,11 @@ end
 
 def selection(num)
   if num = 1
-     
+     binding.pry
       "YUM YUM MUNCH MUNCH MUNCH"
   
   elsif num = 2
-  
+    binding.pry
     "HAM HAM HAM IN MY TUMMY"
   end
 end
